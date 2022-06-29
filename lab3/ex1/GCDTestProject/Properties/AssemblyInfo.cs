0328b7642ebb96cef79b5d214e622b116bcc1d64
ollinn <olgachub1024@gmail.com>
2022-06-29 23:42:25 +0300

    course

---
+++ lab3/ex1/GCDTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GCDTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GCDTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e474b3fc-bfb2-4db5-8dbf-83e0f49054ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ lab3/ex1/GCDTestProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using GreatestCommonDivisor;
+
+namespace GCDTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FindGCDEuclidTest()
+        {
+            int a = 2806; // TODO: Initialize to an appropriate value
+            int b = 345; // TODO: Initialize to an appropriate value
+            int expected = 23; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = GCDAlgorithms.FindGCDEuclid(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
